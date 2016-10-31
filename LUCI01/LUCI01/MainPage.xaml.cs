3b65a1d8238cd229cb381231bdb5742b50fa37c1
Pedro Orgeira <poc.desarrollo@hotmail.com>
2016-10-31 20:23:07 +0100

    Inicio

---
+++ LUCI01/LUCI01/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Speech.Synthesis;
+using Windows.Media.SpeechRecognition;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LUCI01
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        SpeechSynthesizer sinte = new SpeechSynthesizer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            await sinte.SynthesizeTextToStreamAsync("Prueba");
+        }
+    }
+}
